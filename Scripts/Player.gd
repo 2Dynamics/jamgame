e14835fd6cd386feb49251f8508637a38641b105
Tomasz Chabora <kobewi4e@gmail.com>
2020-11-27 21:00:12 +0100

    kolizja z terenem niby

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -10,14 +10,15 @@ func _physics_process(delta: float) -> void:
 	move.x = int(Input.is_action_pressed(action("right"))) - int(Input.is_action_pressed(action("left")))
 	move.y = int(Input.is_action_pressed(action("down"))) - int(Input.is_action_pressed(action("up")))
 	
-	var grav = global_position.direction_to(center.global_position)
+	var grav = global_position.direction_to(globals.center.global_position)
 	rotation = lerp_angle(rotation, grav.angle() - PI/2, 0.1)
 	
-	
-	if global_position.distance_to(center.global_position) > 200: #tymczasowe
+	var raycast = globals.map.raycast(global_position, global_position + grav * 32)
+	if not raycast.collision:
 		velocity += grav * 1000 * delta
 	else:
-		velocity= Vector2(0,0)
+		velocity= Vector2()
+	
 	velocity += move * 400 * delta
 	velocity *= 0.95
 	
--- globals.gd
+++ globals.gd
@@ -1,6 +1,4 @@
 extends Node2D
 
-func _ready() -> void:
-	var center = $Center
-	for node in get_tree().get_nodes_in_group("falling"):
-		node.center = center
+var center: Node2D
+var map: DynamicGridMap
