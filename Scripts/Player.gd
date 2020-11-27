c4650f12140abead08b66106b18fc2c9932c7eca
Tomasz Chabora <kobewi4e@gmail.com>
2020-11-27 18:32:58 +0100

    Gracz czy coś

---
+++ Scripts/Player.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+export var player: int
+
+var center: Node2D
+var velocity: Vector2
+
+func _physics_process(delta: float) -> void:
+	var move: Vector2
+	move.x = int(Input.is_action_pressed(action("right"))) - int(Input.is_action_pressed(action("left")))
+	move.y = int(Input.is_action_pressed(action("down"))) - int(Input.is_action_pressed(action("up")))
+	
+	velocity += global_position.direction_to(center.global_position) * 1000 * delta
+	velocity += move * 400 * delta
+	
+	position += velocity * delta
+
+func action(action: String):
+	return str("p", player, "_", action)
