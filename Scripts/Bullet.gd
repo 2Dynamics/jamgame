af800b4bc203e739e630a9f25049b3fa23a02011
Kamil <kamil_p123@op.pl>
2020-11-27 21:43:37 +0100

    changed Bullet logic; added Enemy to scene

--- Scripts/Bullet.gd
+++ Scripts/Bullet.gd
@@ -10,18 +10,20 @@ var gravicy_accel_val := 98.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
+	center_of_gravity = globals.center.global_position
 	pass # Replace with function body.
 
 func _physics_process(delta):
-	accel += (center_of_gravity-position).normalized() * gravicy_accel_val
+	accel += (center_of_gravity-global_position).normalized() * gravicy_accel_val
 	position += velocity * delta + (accel * delta * delta) / 2
 	velocity += accel * delta
 	accel = Vector2.ZERO
 	rotation = (velocity.angle())
 
 	if collideWithMap():
-		#queue_free()
+		globals.map.update_grid_penetrating_explosive_damage_circle(global_position, 100, 1000000, 0.5)
+		globals.map.update_texture(global_position, Vector2(100, 100))
+		queue_free()
 		# todo
 		pass
 	pass
@@ -31,5 +33,5 @@ func _process(delta):
 	pass
 
 func collideWithMap():
-	
+	return globals.map.is_pixel_solid_v(global_position)
 	pass
