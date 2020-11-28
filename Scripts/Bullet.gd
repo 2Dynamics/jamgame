49c73b5455f691f7fbc839f1d7de753dc12da212
szamq <szamankf@gmail.com>
2020-11-28 14:50:11 +0100

    wybuchy i fixy

--- Scripts/Bullet.gd
+++ Scripts/Bullet.gd
@@ -1,6 +1,8 @@
 extends Node2D
 class_name Bullet
 
+var explosion_scene=preload("res://Nodes/explosion.tscn")
+
 var shooter_id=-1
 var velocity := Vector2.ZERO
 var dmg := 0
@@ -23,6 +25,10 @@ func _physics_process(delta):
 
 	if collideWithMap():
 		globals.draw_explosion(global_position, dmg_radius, dmg)
+		var expl=explosion_scene.instance()
+		get_parent().add_child(expl)
+		expl.global_position=global_position
+
 		queue_free()
 		# todo
 	pass
@@ -41,5 +47,10 @@ func _on_Bullet_area_entered(area):
 		if shooter_id!=area.player:
 			area.setStun()
 			globals.draw_explosion(global_position, dmg_radius, dmg)
+			var expl=explosion_scene.instance()
+			get_parent().add_child(expl)
+			expl.global_position=global_position
+
+			
 			queue_free()
 	pass # Replace with function body.
