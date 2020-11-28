de7298cbbcefc8de4092c3d9188f22f8c7d09d2f
John Meadow <armitage3@gazeta.pl>
2020-11-28 11:47:27 +0100

    Updated Enemy AI

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -6,9 +6,13 @@ var bullet_object := preload("res://Nodes/Bullet.tscn")
 export var life := 600.0
 
 export var shoot_delay := 5.0
+export var bullet_speed:= 100.0
 
 export var spawn_distance := 500.0
 export var orbit_distance := 300.0
+export var orbit_variance := 10.0
+export var orbit_speed := 0.1
+
 
 var velocity := Vector2.ZERO
 
@@ -31,26 +35,28 @@ func _ready():
 	spawn()
 
 func _physics_process(delta):
-#	if is_in_orbit:
-	orbit_angle += orbit_direction * 0.01
+	if !is_in_orbit:
+		if position.distance_squared_to(orbit_position) < 1000:
+			is_in_orbit = true
+			$Timer.start()
+
+	orbit_angle += orbit_direction * deg2rad(orbit_speed)
 	orbit_position = Vector2( cos(orbit_angle), sin(orbit_angle) ) * orbit_distance
-		
-#	else:
-#		if position.distance_squared_to(orbit_position) < 1000:
-#			is_in_orbit = true
 	
-	velocity += (orbit_position - position) * speed
+	var distance_from_desired_orbit = orbit_position - position
+	
+	velocity += distance_from_desired_orbit.normalized() * min(distance_from_desired_orbit.length(),100) * speed
 	velocity *= 0.97
 	position += velocity * delta
 #	position = lerp(position, orbit_position, 0.01)
 	
 	sprite.rotation = (position.angle())
-	damege()
+#	damege()
 		
 		
 func spawn():
 	$explosion.rotation = rand_range(0,TAU) 
-	orbit_distance += rand_range(-10,10)
+	orbit_distance += rand_range(-orbit_variance,orbit_variance)
 	speed += rand_range(-0.01,0.01)
 	orbit_angle = rand_range( 0, TAU )
 	position = Vector2( cos(orbit_angle), sin(orbit_angle) ) * spawn_distance
@@ -62,7 +68,7 @@ func spawn():
 func shoot():
 	var new_bullet := bullet_object.instance() as Bullet
 	get_parent().add_child( new_bullet )
-	new_bullet.velocity = - position.normalized().rotated(-PI/2.0) * 100
+	new_bullet.velocity = - position.normalized().rotated(-PI/2.0) * bullet_speed
 	new_bullet.position = position
 	new_bullet.dmg = 20000
 	new_bullet.dmg_radius = 100
