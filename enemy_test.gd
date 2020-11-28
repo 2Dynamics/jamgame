cef73c8da0cc05b6be705e9c81028aa26a28d15c
John Meadow <armitage3@gazeta.pl>
2020-11-28 21:08:47 +0100

    enemy testing

--- enemy_test.gd
+++ enemy_test.gd
@@ -2,6 +2,12 @@ extends Node2D
 
 var enemy_object = preload("res://Nodes/Enemy.tscn")
 
+func _ready() -> void:
+	randomize()
+	globals.center = $Center
+	globals.gravity_center = $Center.position
+	globals.center_of_gravity = $Center.position
+
 func _on_Timer_timeout():
 	var new_enemy = enemy_object.instance()
 	add_child(new_enemy)
